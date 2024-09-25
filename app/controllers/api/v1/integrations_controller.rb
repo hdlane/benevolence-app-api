ba91650a7433c1595c1ac41ea8ae3498b65f12f0
Hayden Lane <hayden.inbox@gmail.com>
2024-09-25 12:53:33 -0500

    Add OAuth token creation service. Fix typos and spacing. Remove comments.

--- app/controllers/api/v1/integrations_controller.rb
+++ app/controllers/api/v1/integrations_controller.rb
@@ -1,36 +1,20 @@
-require "oauth2"
 require "pco_api"
 
-OAUTH_APP_ID = Rails.application.credentials.oauth_app_id
-OAUTH_SECRET = Rails.application.credentials.oauth_secret
-SCOPE = Rails.application.credentials.scope
-API_URL = Rails.application.credentials.api_url
-DOMAIN = Rails.application.credentials.domain
+API_URL ||= Rails.application.credentials.api_url
+CLIENT_DOMAIN ||= Rails.application.credentials.client_domain
 
 class Api::V1::IntegrationsController < ApplicationController
   def oauth
-    url = client.auth_code.authorize_url(
-      scope: SCOPE,
-      redirect_uri: "#{DOMAIN}/oauth/complete"
-    )
+    url = oauth_token_creation.get_authorize_url
     redirect_to(url, allow_other_host: true)
   end
 
   def oauth_complete
-    token = client.auth_code.get_token(
-      params[:code],
-      redirect_uri: "#{DOMAIN}/oauth/complete"
-    )
-    render json: { token: token }
-    # TODO: How do I get the token from here to a secure cookie
-    # then redirect_to "http://localhost:5173"
+    token = oauth_token_creation.get_token(params[:code])
+    redirect_to(CLIENT_DOMAIN)
   end
 
   def sync
-    # TODO:
-    # * Gather info from PCO to create Person - make a class?
-    # * Create / Update each person from PCO into People table
-
     token = params[:token]
     response = api(token).people.v2.people.get
     people = response["data"]
@@ -42,8 +26,8 @@ class Api::V1::IntegrationsController < ApplicationController
   end
 
   private
-    def client
-      OAuth2::Client.new(OAUTH_APP_ID, OAUTH_SECRET, site: API_URL)
+    def oauth_token_creation
+      OauthTokenCreation.new()
     end
 
     def token
---
+++ app/services/oauth_token_creation.rb
@@ -0,0 +1,30 @@
+require "oauth2"
+
+OAUTH_APP_ID ||= Rails.application.credentials.oauth_app_id
+OAUTH_SECRET ||= Rails.application.credentials.oauth_secret
+SCOPE ||= Rails.application.credentials.scope
+SERVER_DOMAIN ||= Rails.application.credentials.server_domain
+
+class OauthTokenCreation
+  def get_authorize_url
+    url = client.auth_code.authorize_url(
+      scope: SCOPE,
+      redirect_uri: "#{SERVER_DOMAIN}/oauth/complete"
+    )
+    return url
+  end
+
+  def get_token(code)
+    token = client.auth_code.get_token(
+      code,
+      redirect_uri: "#{SERVER_DOMAIN}/oauth/complete"
+    )
+    # render json: { token: token }
+    return token
+  end
+
+  private
+    def client
+      OAuth2::Client.new(OAUTH_APP_ID, OAUTH_SECRET, site: API_URL)
+    end
+end
