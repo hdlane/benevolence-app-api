e8bb373547812c9fc65f9ff4a8dd8f74d8a2205f
Hayden Lane <hayden.inbox@gmail.com>
2024-09-27 17:01:01 -0500

    Remove cascade based on person

--- db/migrate/20240923195332_create_requests.rb
+++ db/migrate/20240923195332_create_requests.rb
@@ -1,7 +1,7 @@
 class CreateRequests < ActiveRecord::Migration[7.2]
   def change
     create_table :requests do |t|
-      t.references :person, null: false, foreign_key: { on_delete: :cascade }, index: true
+      t.references :person, null: false, foreign_key: true, index: true
       t.references :organization, null: false, foreign_key: { on_delete: :cascade }, index: true
       t.string :type, null: false
       t.string :title, null: false
@@ -18,6 +18,6 @@ class CreateRequests < ActiveRecord::Migration[7.2]
 
       t.timestamps
     end
-    add_index :requests, [:person_id, :organization_id]
+    add_index :requests, [ :person_id, :organization_id ]
   end
 end
