b3b15053e6ecee5fa8066bade4e4ce7a82ea24fd
Hayden Lane <hayden.inbox@gmail.com>
2024-11-14 21:59:18 -0600

    Update people migration.

--- db/migrate/20240923195106_create_people.rb
+++ db/migrate/20240923195106_create_people.rb
@@ -1,7 +1,7 @@
 class CreatePeople < ActiveRecord::Migration[7.2]
   def change
     create_table :people do |t|
-      t.references :organization, null: false, foreign_key: { on_delete: :cascade }, index: true
+      t.references :organizations, null: false, foreign_key: { on_delete: :cascade }, index: true
       t.string :first_name, null: false
       t.string :last_name, null: false
       t.string :name, null: false
