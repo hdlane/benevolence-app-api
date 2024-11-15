ef2c219cd28107e686724be5d2a622b21d5e87f4
Hayden Lane <hayden.inbox@gmail.com>
2024-11-14 20:52:51 -0600

    Update env variables and change database to postgresql.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# SendGrid email configuration
+ActionMailer::Base.smtp_settings = {
+  user_name: "apikey",
+  password: Rails.application.credentials.sendgrid_api_key,
+  domain: "benevolenceapp.com",
+  address: "smtp.sendgrid.net",
+  port: 587,
+  authentication: :plain,
+  enable_starttls_auto: true
+}
